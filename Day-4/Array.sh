79a10cf02209fe4af51cfd10fad730368a87f325
Progyan Bhattacharya <progyanb@acm.org>
2017-10-16 16:30:17 +0530

    16-10-2017: Array and Sum

--- Day-4/Array.sh
+++ Day-4/Array.sh
@@ -1,3 +1,11 @@
 echo "Enter the number of terms"
 read n
-for
+sum=0
+echo "Enter $n numbers"
+for i in $(seq 1 $n)
+do
+    read a
+    arr[$i]=$a
+    sum=`expr $sum + $a`
+done
+echo "Sum of all elements i array is: $sum"
